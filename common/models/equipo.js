a93502f55919292817fcc8cd31bfaa6acc95ab97
cdanmontoya <carlos.danmontoya@gmail.com>
2019-10-18 09:14:21 -0500

    adding get distances for separated teams

--- common/models/equipo.js
+++ common/models/equipo.js
@@ -2,9 +2,17 @@
 var app = require('../../server/server');
 const fetch = require('node-fetch');
 
+const calcularDistancia = (origen, destino) => {
+    const R = 6378;
+    
+    let deltaLat = destino.latitud - origen.latitud;
+    let deltaLong = destino.longitud - origen.longitud;
+}
+
+
 module.exports = function(Equipo) {
-    Equipo.asignarSede = async function (users, juntos) {
-        let API_KEY = ''
+    Equipo.asignarSede = async function (users, juntos, callback) {
+        let API_KEY = 'AIzaSyBFsf5rZfPbS1_i_8CmwTJLYCFzTo_yLDo';
         let origins = '';
         let destinations = '';
         
@@ -25,14 +33,54 @@ module.exports = function(Equipo) {
 
         const query = `https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=${origins}&destinations=${destinations}&key=${API_KEY}`
 
-        fetch(query)
-            .then(checkStatus)
-            .then(res => {console.log(`will not get here... ${res}`) 
-                console.log(res)
-            })
-            .catch(err => console.log(err))
+        let distances = await fetch(query)
+        distances = await distances.json()
+        let rows = distances.rows
 
-        } 
+        let _rows = rows.length
+        let mejoresSedes = [];
+        
+        if (juntos === 'false') {
+            
+            for (let i = 0; i < _rows; i++) {
+                let row = rows[i].elements;
+                let _columns = row.length;
+
+                let minDistance = row[0].distance.value
+                let minIndex = 0;
+                console.log(minDistance)
+
+                for (let j = 0; j < _columns; j++) {
+                    let actualDistance = row[i].distance.value;
+                    if (actualDistance < minDistance) {
+                        minDistance = actualDistance;
+                        minIndex = j;
+                    }
+                }
+                mejoresSedes.push(minIndex)
+            } 
+
+            console.log(mejoresSedes)
+
+            let myRes = []
+
+            for (let i = 0; i < mejoresSedes.length; i++) {
+                myRes.push(distances.destination_addresses[mejoresSedes[i]])
+            }
+
+            callback(null, myRes)
+        }
+
+        // fetch(query).then((res) => {
+        //     return res.json();
+            
+        // }).then((myJSON) => {
+        //     console.log(myJSON.rows);
+            
+        // })
+
+        // console.log(distances)
+        
         
 
     }
@@ -46,7 +94,7 @@ module.exports = function(Equipo) {
             { arg: 'users', type: 'array' },
             {arg: 'juntos', type: 'string'}
         ],
-        returns: [{arg: 'jeje', type: 'string'}]
+        returns: {arg: 'response', type: 'string'}
     })
 
 };
