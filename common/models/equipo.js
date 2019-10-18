3c2a789ed91ab53729d8dde38a433874ff0f2577
cdanmontoya <carlos.danmontoya@gmail.com>
2019-10-17 22:29:20 -0500

    Adding base

--- common/models/equipo.js
+++ common/models/equipo.js
@@ -1,5 +1,18 @@
 'use strict';
 
 module.exports = function(Equipo) {
+    Equipo.asignarSede = (users) => {
+
+    }
 
 };
+
+Equipo.remoteMethod('asignarSede', {
+    http: {
+        path: '/asignar-sede',
+        verb: 'post'
+    },
+    accepts: [
+        { arg: 'users', type: 'array' }
+    ]
+})
