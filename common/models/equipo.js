863b0e98afe240d5e5bc8d9adef74a3fbeb5808e
cdanmontoya <carlos.danmontoya@gmail.com>
2019-10-18 00:05:15 -0500

    Some advances

--- common/models/equipo.js
+++ common/models/equipo.js
@@ -2,17 +2,20 @@
 
 module.exports = function(Equipo) {
     Equipo.asignarSede = (users) => {
+        console.log(users);
+        
 
     }
 
+    Equipo.remoteMethod('asignarSede', {
+        http: {
+            path: '/asignar-sede',
+            verb: 'post'
+        },
+        accepts: [
+            { arg: 'users', type: 'array' }
+        ]
+    })
+
 };
 
-Equipo.remoteMethod('asignarSede', {
-    http: {
-        path: '/asignar-sede',
-        verb: 'post'
-    },
-    accepts: [
-        { arg: 'users', type: 'array' }
-    ]
-})
